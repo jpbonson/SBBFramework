6323f06bc03310ed9a0e3b67d8a49239c270f334
jpbonson <jpbonson@gmail.com>
2015-06-15 02:58:56 -0300

    updated exmaples so now they are compatible with the new configs

--- SBB/examples/thyroid_config.py
+++ SBB/examples/thyroid_config.py
@@ -3,13 +3,7 @@ import copy
 """
 ##### Results using Mann-Whitney U-Test (http://www.socscistatistics.com/tests/mannwhitney/Default2.aspx):
 
-THYROID_DEFAULT < THYROID_WITH_PARETOS: (old)
-The U-value is 168. The distribution is approximately normal. Therefore, the Z-value above can be used.
-The Z-Score is -2.794. The p-value is 0.00528. The result is significant at p <= 0.05.
 
-THYROID_WITH_PARETOS < THYROID_WITH_PARETO_FOR_TEAMS_ONLY: (old)
-The U-value is 197. The distribution is approximately normal. Therefore, the Z-value above can be used.
-The Z-Score is -2.2313. The p-value is 0.02574. The result is significant at p <= 0.05.
 
 """
 
@@ -24,8 +18,7 @@ THYROID_DEFAULT = {
         'generations_total': 300,
         'validate_after_each_generation': 50,
         'populations': {
-            'programs': 120,
-            'teams': 60,
+            'teams': 80,
             'points': 120,
         },
         'replacement_rate': {
@@ -35,20 +28,22 @@ THYROID_DEFAULT = {
         'mutation': {
             'team': {
                 'remove_program': 0.7,
-                'add_program': 0.8,
+                'add_program': 0.7,
+                'mutate_program': 0.2, # is applied to all programs in the team, until at least one program is mutated
             },
             'program': {
-                'remove_instruction': 0.7,
-                'add_instruction': 0.8,
-                'change_instruction': 0.8,
+                'remove_instruction': 0.5,
+                'add_instruction': 0.5,
+                'change_instruction': 1.0,
+                'swap_instructions': 1.0,
                 'change_action': 0.1,
             },
         },
         'team_size': { # the min size is the total number of actions
+            'min': 2,
             'max': 9,
         },
         'program_size': {
-            'initial': 10,
             'min': 2,
             'max': 20,
         },
@@ -73,70 +68,12 @@ THYROID_DEFAULT = {
                 'p_value': 0.1,
             },       
         },
+        'run_initialization_step2': False,
     },
 }
 """
-#################### OVERALL RESULTS #################### (old)
+#################### OVERALL RESULTS ####################
 
-Test Score per Run: [0.73487, 0.64663, 0.81808, 0.64663, 0.97086, 0.88397, 0.59611, 0.87215, 0.88763, 0.88425, 0.93716, 0.93864, 0.85918, 0.87286, 0.90614, 0.64663, 0.77507, 0.64663, 0.92979, 0.93025, 0.95475, 0.93108, 0.95831, 0.67403, 0.96206]
-mean: 0.83455
-std. deviation: 0.12124
 
-Train Score per Generation across Runs:
-mean: [0.525, 0.70866, 0.73999, 0.78133, 0.79633, 0.81866, 0.83166]
-std. deviation: [0.07582, 0.1051, 0.09507, 0.1212, 0.11125, 0.10972, 0.11303]
-
-Test Score per Generation across Runs:
-mean: [0.52352, 0.70024, 0.74573, 0.77426, 0.78983, 0.80986, 0.83455]
-std. deviation: [0.08604, 0.09477, 0.09776, 0.1187, 0.11562, 0.10995, 0.12124]
-
-Finished execution, total elapsed time: 16250.383 secs (mean: 650.01532, std: 179.87195)
-
-"""
-
-THYROID_WITH_PARETOS = copy.deepcopy(THYROID_DEFAULT)
-THYROID_WITH_PARETOS['advanced_training_parameters']['use_pareto_for_team_population_selection'] = True
-THYROID_WITH_PARETOS['advanced_training_parameters']['use_pareto_for_point_population_selection'] = True
-"""
-#################### OVERALL RESULTS #################### (old)
-
-Test Score per Run: [0.64459, 0.6969, 0.96019, 0.7823, 0.77515, 0.94043, 0.97135, 0.85577, 0.93783, 0.93661, 0.8784, 0.94235, 0.90138, 0.97607, 0.87257, 0.97954, 0.90812, 0.93232, 0.93648, 0.96286, 0.87725, 0.72491, 0.95513, 0.90305, 0.96395]
-mean: 0.88862
-std. deviation: 0.09112
-
-Train Score per Generation across Runs:
-mean: [0.55799, 0.76033, 0.795, 0.83066, 0.83266, 0.84566, 0.85233]
-std. deviation: [0.06512, 0.08118, 0.06227, 0.06154, 0.06441, 0.07257, 0.07492]
-
-Test Score per Generation across Runs:
-mean: [0.56046, 0.76837, 0.83837, 0.86429, 0.87533, 0.869, 0.88862]
-std. deviation: [0.06466, 0.11167, 0.10988, 0.09894, 0.09192, 0.09477, 0.09112]
-
-Finished execution, total elapsed time: 14872.799 secs (mean: 594.91196, std: 2113.68824)
-"""
-
-THYROID_WITH_PARETO_FOR_TEAMS_ONLY = copy.deepcopy(THYROID_DEFAULT)
-THYROID_WITH_PARETO_FOR_TEAMS_ONLY['advanced_training_parameters']['use_pareto_for_team_population_selection'] = True
-"""
-#################### OVERALL RESULTS #################### (old)
-
-Test Score per Run: [0.82456, 0.97561, 0.95241, 0.9617, 0.95283, 0.90201, 0.97351, 0.98405, 0.9754, 0.90388, 0.9351, 0.9754, 0.98137, 0.97534, 0.93341, 0.95244, 0.94634, 0.95919, 0.88091, 0.97351, 0.92812, 0.9754, 0.92768, 0.93859, 0.84532]
-mean: 0.94136
-std. deviation: 0.04116
-
-Train Score per Generation across Runs:
-mean: [0.55033, 0.83, 0.88466, 0.91633, 0.93433, 0.93766, 0.95]
-std. deviation: [0.07765, 0.07986, 0.07366, 0.0578, 0.05604, 0.05627, 0.03162]
-
-Test Score per Generation across Runs:
-mean: [0.5472, 0.80719, 0.87172, 0.88456, 0.90929, 0.92799, 0.94136]
-std. deviation: [0.07371, 0.09, 0.08718, 0.08507, 0.07379, 0.05454, 0.04116]
-
-Finished execution, total elapsed time: 17018.09599 secs (mean: 680.72383, std: 82.18161)
-"""
-
-THYROID_WITH_PARETO_FOR_POINTS_ONLY = copy.deepcopy(THYROID_DEFAULT)
-THYROID_WITH_PARETO_FOR_POINTS_ONLY['advanced_training_parameters']['use_pareto_for_point_population_selection'] = True
-"""
 
 """
--- SBB/examples/tictactoe_config.py
+++ SBB/examples/tictactoe_config.py
@@ -6,6 +6,7 @@ import copy
 
 
 """
+
 TICTACTOE_DEFAULT = {
     'task': 'reinforcement',
     'reinforcement_parameters': { # only used if 'task' is 'reinforcement'
@@ -13,15 +14,19 @@ TICTACTOE_DEFAULT = {
         'validation_population': 100, # at a validated generation, all the teams with be tested against this population, the best one is the champion
         'champion_population': 1000, # at a validated generation, these are the points the champion team will play against to obtain the metrics
         'opponents_pool': 'only_coded',
+        'balanced_opponent_populations': True, # if false, the opponent populations will be swapped instead of mixed
+        'hall_of_fame': {
+            'enabled': True,
+            'use_genotype_diversity': True, # if False, use the fitness as the criteria to remove teams when the Hall of Fame is full
+        },
         'print_matches': False, # use this option to debug
     },
 
     'training_parameters': {
-        'runs_total': 5,
-        'generations_total': 200,
+        'runs_total': 10,
+        'generations_total': 300,
         'validate_after_each_generation': 50,
         'populations': {
-            'programs': 120,
             'teams': 60,
             'points': 60,
         },
@@ -32,20 +37,22 @@ TICTACTOE_DEFAULT = {
         'mutation': {
             'team': {
                 'remove_program': 0.7,
-                'add_program': 0.8,
+                'add_program': 0.7,
+                'mutate_program': 0.2, # is applied to all programs in the team, until at least one program is mutated
             },
             'program': {
-                'remove_instruction': 0.7,
-                'add_instruction': 0.8,
-                'change_instruction': 0.8,
+                'remove_instruction': 0.5,
+                'add_instruction': 0.5,
+                'change_instruction': 1.0,
+                'swap_instructions': 1.0,
                 'change_action': 0.1,
             },
         },
         'team_size': { # the min size is the total number of actions
+            'min': 2,
             'max': 18,
         },
         'program_size': {
-            'initial': 10,
             'min': 2,
             'max': 20,
         },
@@ -70,38 +77,12 @@ TICTACTOE_DEFAULT = {
                 'p_value': 0.1,
             },       
         },
+        'run_initialization_step2': False,
     },
 }
 """
-#################### OVERALL RESULTS #################### point_population = 90 (old)
+#################### OVERALL RESULTS ####################
 
-Test Score per Run: [0.68175, 0.6755, 0.71575, 0.6755, 0.69725]
-mean: 0.68915
-std. deviation: 0.01549
 
-Train Score per Generation across Runs:
-mean: [0.52388, 0.67666, 0.68, 0.71666, 0.68]
-std. deviation: [0.05498, 0.04569, 0.0311, 0.02971, 0.03614]
 
-Test Score per Generation across Runs:
-mean: [0.4969, 0.66485, 0.67045, 0.68739, 0.68915]
-std. deviation: [0.04828, 0.01884, 0.0151, 0.01643, 0.01549]
-
-Finished execution, total elapsed time: 16958.914 secs (mean: 3391.7828, std: 214.35365)
-
-#################### OVERALL RESULTS #################### point_population = 30 (old)
-
-Test Score per Run: [0.6745, 0.672, 0.64249, 0.68725, 0.6745]
-mean: 0.67014
-std. deviation: 0.01482
-
-Train Score per Generation across Runs:
-mean: [0.48166, 0.66833, 0.64333, 0.67833, 0.66166]
-std. deviation: [0.05807, 0.05436, 0.04755, 0.01943, 0.07257]
-
-Test Score per Generation across Runs:
-mean: [0.48605, 0.63205, 0.63849, 0.67005, 0.67015]
-std. deviation: [0.04427, 0.02169, 0.02574, 0.0117, 0.01482]
-
-Finished execution, total elapsed time: 6237.51399 secs (mean: 1247.50279, std: 154.82678)
 """
