d5cc205938b003385f137c829ed2881fa396dc2e
jpbonson <jpbonson@gmail.com>
2015-06-22 20:47:49 -0300

    refactored poker match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from SBB.sbb import SBB
 from SBB.config import Config
+from SBB.environments.poker.poker_match import PokerMatch
 
 if __name__ == "__main__":
     Config.check_parameters()
     SBB().run()
+    # PokerMatch().run()
