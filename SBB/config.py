a6847a6adcff86590575643025fdf5f022d4dcee
jpbonson <jpbonson@gmail.com>
2015-07-06 15:34:28 -0300

    config seems to work

--- SBB/config.py
+++ SBB/config.py
@@ -18,8 +18,8 @@ class Config():
         'reinforcement_parameters': { # only used if 'task' is 'reinforcement'
             'environment': 'poker', # edit _initialize_environment() in SBB and RESTRICTIONS['environment_types'] to add new environments (they must implement DefaultEnvironment)
             'validation_population': 60, # at a validated generation, all the teams with be tested against this population, the best one is the champion
-            'champion_population': 150, # at a validated generation, these are the points the champion team will play against to obtain the metrics
-            'opponents_pool': 'only_sbb',
+            'champion_population': 180, # at a validated generation, these are the points the champion team will play against to obtain the metrics
+            'opponents_pool': 'only_coded',
             'balanced_opponent_populations': True, # if False, the opponent populations will be swapped instead of mixed
             'hall_of_fame': {
                 'enabled': False,
@@ -27,16 +27,16 @@ class Config():
             },
             'debug_matches': False, # use this option to debug
             'poker': {
-                'total_hands': 20,
+                'total_hands': 2,
             }
         },
 
         'training_parameters': {
             'runs_total': 1,
-            'generations_total': 100,
+            'generations_total': 200,
             'validate_after_each_generation': 20,
             'populations': {
-                'teams': 100,
+                'teams': 60,
                 'points': 30,
             },
             'replacement_rate': {
