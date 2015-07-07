b626e845635e118597f462aafea460b39950b734
jpbonson <jpbonson@gmail.com>
2015-07-06 21:03:54 -0300

    remvoed useless reference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from SBB.sbb import SBB
 from SBB.config import Config
-from SBB.environments.poker.poker_environment import PokerEnvironment
 
 if __name__ == "__main__":
     Config.check_parameters()
