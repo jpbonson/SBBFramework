d5b17dacf715ebdca6bed96efc8dd3c41b153c16
jpbonson <jpbonson@gmail.com>
2016-09-07 14:39:33 -0300

    ttt with sockets is working, TODO: clean code, generalize code

--- SBB/environments/reinforcement_environment_for_sockets.py
+++ SBB/environments/reinforcement_environment_for_sockets.py
@@ -14,8 +14,7 @@ from ..core.pareto_dominance_for_teams import ParetoDominanceForTeams
 from ..utils.helpers import round_value, flatten, accumulative_performances, rank_teams_by_accumulative_score
 from ..config import Config
 
-# TODO
-from tictactoe.tictactoe_match import TictactoeMatch
+# AQUI
 from tictactoe.tictactoe_opponents import TictactoeRandomOpponent, TictactoeSmartOpponent
 
 class ReinforcementEnvironmentForSockets(ReinforcementEnvironment):
@@ -23,20 +22,23 @@ class ReinforcementEnvironmentForSockets(ReinforcementEnvironment):
     
     """
 
-    # TODO: implementar tictactoe_game com tictactoe_environment para sockets
+    # TODO:
+    # - implementar tictactoe_game com tictactoe_environment para sockets
+    # - limpar codigo de requests
     # - fazer arquivo externo para config
+    # - atualizar testes para usar setUp e tearDown
+    # - fazer reinforcement para sockets ser generico
+    # - fazer oponentes se comunicarem via socket
 
     def __init__(self):
-        # super(ReinforcementEnvironmentForSockets, self).__init__(total_actions, total_inputs, total_labels, coded_opponents_for_training, coded_opponents_for_validation, point_class)
-        
-        # atualizar testes para usar setUp e tearDown
-
         total_actions = 9 # spaces in the board
         total_inputs = 9 # spaces in the board (0, 1, 2 as the states, 0: no player, 1: player 1, 2: player 2)
         total_labels = 1 # since no labels are being used, group everything is just one label
-        coded_opponents = [TictactoeRandomOpponent, TictactoeSmartOpponent]
+        coded_opponents_for_training = [TictactoeRandomOpponent, TictactoeSmartOpponent]
+        coded_opponents_for_validation = [TictactoeRandomOpponent, TictactoeSmartOpponent]
         point_class = ReinforcementPoint
-        super(ReinforcementEnvironmentForSockets, self).__init__(total_actions, total_inputs, total_labels, coded_opponents, coded_opponents, point_class)
+        super(ReinforcementEnvironmentForSockets, self).__init__(total_actions, total_inputs, total_labels, coded_opponents_for_training, coded_opponents_for_validation, point_class)
+        
         self.total_positions_ = 2
         self.action_mapping_ = {
             '[0,0]': 0, '[0,1]': 1, '[0,2]': 2,
@@ -54,19 +56,88 @@ class ReinforcementEnvironmentForSockets(ReinforcementEnvironment):
         self.connection.send(json.dumps(message))
         print "\nConnection established.\n"
 
-    def _request_match(self, team, opponent, point, mode, match_id):
-        # parameters:
-        #   team? acessado por uma port?
-        #   opponent? acessado por uma port?
-        #   point?
+    def _play_match(self, team, opponent, point, mode, match_id):
+        """
+        
+        """
+        
+        if mode == Config.RESTRICTIONS['mode']['training']:
+            is_training = True
+        else:
+            is_training = False
+        outputs = []
+        for position in range(1, self.total_positions_+1):
+            if position == 1:
+                first_player = opponent
+                is_training_for_first_player = False
+                second_player = team
+                is_training_for_second_player = is_training
+                sbb_player = 2
+            else:
+                first_player = team
+                is_training_for_first_player = is_training
+                second_player = opponent
+                is_training_for_second_player = False
+                sbb_player = 1
 
+            self._request_match(team, opponent, point, mode, match_id, 
+                player1_label = first_player.__repr__(), player2_label = second_player.__repr__())
+
+            opponent.initialize(point.seed_)
+            while True:
+                player = 1
+
+                inputs = self._request_inputs_from_the_point_of_view_of(player, mode, match_id)
+
+                valid_actions = self._request_valid_actions(mode, match_id)
+
+                action = first_player.execute(point.point_id_, inputs, valid_actions, is_training_for_first_player)
+                if action is None:
+                    action = random.choice(valid_actions)
+
+                if is_training_for_first_player:
+                    first_player.action_sequence_['coding2'].append(str(action))
+                    first_player.action_sequence_['coding4'].append(str(action))
+
+                self._request_perform_action(player, action, mode, match_id)
+
+                if self._request_is_over(mode, match_id):
+                    result = self._request_result_for_player(sbb_player, mode, match_id)
+                    outputs.append(result)
+                    team.action_sequence_['coding3'].append(int(result*2))
+                    break
+                player = 2
+
+                inputs = self._request_inputs_from_the_point_of_view_of(player, mode, match_id)
+
+                valid_actions = self._request_valid_actions(mode, match_id)
+
+                action = second_player.execute(point.point_id_, inputs, valid_actions, is_training_for_second_player)
+                if action is None:
+                    action = random.choice(valid_actions)
+                if is_training_for_second_player:
+                    second_player.action_sequence_['coding2'].append(str(action))
+                    second_player.action_sequence_['coding4'].append(str(action))
+
+                self._request_perform_action(player, action, mode, match_id)
+
+                if self._request_is_over(mode, match_id):
+                    result = self._request_result_for_player(sbb_player, mode, match_id)
+                    outputs.append(result)
+                    team.action_sequence_['coding3'].append(int(result*2))
+                    break
+        return numpy.mean(outputs)
+
+    def _request_match(self, team, opponent, point, mode, match_id, player1_label, player2_label):
         if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
             print "\nAsking for a new match... match_id: "+str(match_id)+"\n"
         message = {
-            'request': 'new_match',
+            'request_type': 'new_match',
             'request_params': {
-                'mode': mode, # internal use + debug?
-                'match_id': match_id, # for debug
+                'mode': mode, # internal use + debug
+                'match_id': match_id, # debug
+                'player1_label': player1_label,
+                'player2_label': player2_label,
             }
         }
         self.connection.send(json.dumps(message))
@@ -92,89 +163,190 @@ class ReinforcementEnvironmentForSockets(ReinforcementEnvironment):
         if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
             print "\nNew match ready.\n"
 
-    def _play_match(self, team, opponent, point, mode, match_id):
-        """
-        
-        """
-        
-        self._request_match(team, opponent, point, mode, match_id)        
+    def _request_inputs_from_the_point_of_view_of(self, player, mode, match_id):
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nAsking for inputs for player "+str(player)+"... match_id: "+str(match_id)+"\n"
+        message = {
+            'request_type': 'inputs',
+            'request_params': {
+                'mode': mode, # internal use + debug
+                'match_id': match_id, # debug
+                'player': player,
+            }
+        }
+        self.connection.send(json.dumps(message))
 
-        if mode == Config.RESTRICTIONS['mode']['training']:
-            is_training = True
-        else:
-            is_training = False
-        outputs = []
-        for position in range(1, self.total_positions_+1):
-            if position == 1:
-                first_player = opponent
-                is_training_for_first_player = False
-                second_player = team
-                is_training_for_second_player = is_training
-                sbb_player = 2
+        result = []
+        try:
+            ready = select.select([self.connection], [], [self.connection], Config.USER['advanced_training_parameters']['sockets_parameters']['requests_timeout'])
+            if ready[0]:
+                data = self.connection.recv(Config.USER['advanced_training_parameters']['sockets_parameters']['buffer'])
+                if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                    print "data: "+str(data)
+                data = json.loads(data)
+                if 'request_result' in data and data['request_result']:
+                    if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                        print "Request accepted."
+                    result = data['result']['inputs']
+                else:
+                    raise socket.error("Client did not answer with a valid message")
             else:
-                first_player = team
-                is_training_for_first_player = is_training
-                second_player = opponent
-                is_training_for_second_player = False
-                sbb_player = 1
+                raise socket.timeout("Timeout to receive results of requests messages")
+        except Exception as e:
+            print "\n<< It was not possible to connect to the SBB client. >>\n"
+            raise e
 
-            # AQUI: iniciar o app no socket?
-            match = TictactoeMatch(player1_label = first_player.__repr__(), player2_label = second_player.__repr__())
-            
-            opponent.initialize(point.seed_)
-            while True:
-                player = 1
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nInputs ready.\n"
 
-                # AQUI
-                inputs = match.inputs_from_the_point_of_view_of(player)
+        return result
 
-                # AQUI
-                valid_actions = match.valid_actions()
+    def _request_valid_actions(self, mode, match_id):
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nAsking for valid actions... match_id: "+str(match_id)+"\n"
+        message = {
+            'request_type': 'valid_actions',
+            'request_params': {
+                'mode': mode, # internal use + debug
+                'match_id': match_id, # debug
+            }
+        }
+        self.connection.send(json.dumps(message))
 
-                action = first_player.execute(point.point_id_, inputs, valid_actions, is_training_for_first_player)
-                if action is None:
-                    action = random.choice(valid_actions)
+        result = []
+        try:
+            ready = select.select([self.connection], [], [self.connection], Config.USER['advanced_training_parameters']['sockets_parameters']['requests_timeout'])
+            if ready[0]:
+                data = self.connection.recv(Config.USER['advanced_training_parameters']['sockets_parameters']['buffer'])
+                if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                    print "data: "+str(data)
+                data = json.loads(data)
+                if 'request_result' in data and data['request_result']:
+                    if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                        print "Request accepted."
+                    result = data['result']['actions']
+                else:
+                    raise socket.error("Client did not answer with a valid message")
+            else:
+                raise socket.timeout("Timeout to receive results of requests messages")
+        except Exception as e:
+            print "\n<< It was not possible to connect to the SBB client. >>\n"
+            raise e
 
-                if is_training_for_first_player:
-                    first_player.action_sequence_['coding2'].append(str(action))
-                    first_player.action_sequence_['coding4'].append(str(action))
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nValid actions ready.\n"
 
-                # AQUI
-                match.perform_action(player, action)
+        return result
 
-                # AQUI
-                if match.is_over():
-                    # AQUI
-                    result = match.result_for_player(sbb_player)
-                    outputs.append(result)
-                    team.action_sequence_['coding3'].append(int(result*2))
-                    break
-                player = 2
+    def _request_perform_action(self, player, action, mode, match_id):
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nAsking for perform action... match_id: "+str(match_id)+"\n"
+        message = {
+            'request_type': 'perform_action',
+            'request_params': {
+                'mode': mode, # internal use + debug
+                'match_id': match_id, # debug
+                'player': player,
+                'action': action,
+            }
+        }
+        self.connection.send(json.dumps(message))
 
-                # AQUI
-                inputs = match.inputs_from_the_point_of_view_of(player)
+        try:
+            ready = select.select([self.connection], [], [self.connection], Config.USER['advanced_training_parameters']['sockets_parameters']['requests_timeout'])
+            if ready[0]:
+                data = self.connection.recv(Config.USER['advanced_training_parameters']['sockets_parameters']['buffer'])
+                if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                    print "data: "+str(data)
+                data = json.loads(data)
+                if 'request_result' in data and data['request_result']:
+                    if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                        print "Request accepted."
+                else:
+                    raise socket.error("Client did not answer with a valid message")
+            else:
+                raise socket.timeout("Timeout to receive results of requests messages")
+        except Exception as e:
+            print "\n<< It was not possible to connect to the SBB client. >>\n"
+            raise e
 
-                # AQUI
-                valid_actions = match.valid_actions()
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nPerform action ready.\n"
 
-                action = second_player.execute(point.point_id_, inputs, valid_actions, is_training_for_second_player)
-                if action is None:
-                    action = random.choice(valid_actions)
-                if is_training_for_second_player:
-                    second_player.action_sequence_['coding2'].append(str(action))
-                    second_player.action_sequence_['coding4'].append(str(action))
+    def _request_is_over(self, mode, match_id):
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nAsking for is_over... match_id: "+str(match_id)+"\n"
+        message = {
+            'request_type': 'is_over',
+            'request_params': {
+                'mode': mode, # internal use + debug
+                'match_id': match_id, # debug
+            }
+        }
+        self.connection.send(json.dumps(message))
 
-                # AQUI
-                match.perform_action(player, action)
+        result = []
+        try:
+            ready = select.select([self.connection], [], [self.connection], Config.USER['advanced_training_parameters']['sockets_parameters']['requests_timeout'])
+            if ready[0]:
+                data = self.connection.recv(Config.USER['advanced_training_parameters']['sockets_parameters']['buffer'])
+                if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                    print "data: "+str(data)
+                data = json.loads(data)
+                if 'request_result' in data and data['request_result']:
+                    if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                        print "Request accepted."
+                    result = data['result']['is_over']
+                else:
+                    raise socket.error("Client did not answer with a valid message")
+            else:
+                raise socket.timeout("Timeout to receive results of requests messages")
+        except Exception as e:
+            print "\n<< It was not possible to connect to the SBB client. >>\n"
+            raise e
 
-                # AQUI
-                if match.is_over():
-                    # AQUI
-                    result = match.result_for_player(sbb_player)
-                    outputs.append(result)
-                    team.action_sequence_['coding3'].append(int(result*2))
-                    break
-        return numpy.mean(outputs)
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nis_over ready.\n"
+
+        return result
+
+    def _request_result_for_player(self, sbb_player, mode, match_id):
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nAsking for is_over... match_id: "+str(match_id)+"\n"
+        message = {
+            'request_type': 'match_result',
+            'request_params': {
+                'mode': mode, # internal use + debug
+                'match_id': match_id, # debug
+                'player': sbb_player,
+            }
+        }
+        self.connection.send(json.dumps(message))
+
+        result = []
+        try:
+            ready = select.select([self.connection], [], [self.connection], Config.USER['advanced_training_parameters']['sockets_parameters']['requests_timeout'])
+            if ready[0]:
+                data = self.connection.recv(Config.USER['advanced_training_parameters']['sockets_parameters']['buffer'])
+                if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                    print "data: "+str(data)
+                data = json.loads(data)
+                if 'request_result' in data and data['request_result']:
+                    if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+                        print "Request accepted."
+                    result = data['result']['match_result']
+                else:
+                    raise socket.error("Client did not answer with a valid message")
+            else:
+                raise socket.timeout("Timeout to receive results of requests messages")
+        except Exception as e:
+            print "\n<< It was not possible to connect to the SBB client. >>\n"
+            raise e
+
+        if Config.USER['advanced_training_parameters']['sockets_parameters']['debug']:
+            print "\nis_over ready.\n"
+
+        return result
 
     def metrics(self):
         msg = ""
--- SBB/tests/system_tests/tictactoe_game.py
+++ SBB/tests/system_tests/tictactoe_game.py
@@ -85,15 +85,34 @@ class TictactoeGame():
                         if not self._is_valid_request(data):
                             raise socket.error("Server did not send a valid request")
                         else:
-                            if TictactoeGame.CONFIG['debug']:
-                                print "Starting new match."
-                            # TODO
-                            self.player_label[1] = 'p1'
-                            self.player_label[2] = 'p2'
-
                             message = {
                                 'request_result': True,
+                                'result': {},
                             }
+
+                            # TODO: usar cases
+                            if TictactoeGame.CONFIG['debug']:
+                                print "Request type: "+str(data['request_type'])
+
+                            if data['request_type'] == 'new_match':
+                                self._initialize_match(data)
+
+                            if data['request_type'] == 'inputs':
+                                message['result']['inputs'] = self._inputs_from_the_point_of_view_of(data['request_params']['player'])
+                            
+                            if data['request_type'] == 'valid_actions':
+                                message['result']['actions'] = self._valid_actions()
+
+                            if data['request_type'] == 'perform_action':
+                                self._perform_action(data['request_params']['player'], 
+                                    data['request_params']['action'])
+
+                            if data['request_type'] == 'is_over':
+                                message['result']['is_over'] = self._is_over()
+
+                            if data['request_type'] == 'match_result':
+                                message['result']['match_result'] = self._result_for_player(data['request_params']['player'])
+                            
                             self.client_socket.send(json.dumps(message))
             except Exception as e:
                 if TictactoeGame.CONFIG['silent_errors']:
@@ -103,33 +122,22 @@ class TictactoeGame():
                     raise e
 
     def _is_valid_request(self, data):
-        if 'request' in data and data['request'] == 'new_match':
-            return True
+        if 'request_type' in data:
+            if data['request_type'] in ['new_match', 'inputs', 'valid_actions', 
+                'perform_action', 'is_over', 'match_result']:
+                return True
         return False
 
-    def perform_action(self, current_player, action):
-        """
-        Perform the action for the current_player in the board, modifying 
-        the attribute inputs_.
-        """
-        self.inputs_[action] = current_player
-        if self.print_game_:
-            print "---"
-            print str(self.inputs_[0:3])
-            print str(self.inputs_[3:6])
-            print str(self.inputs_[6:9])
+    def _initialize_match(self, data):
+        self.player_label[1] = data['request_params']['player1_label']
+        self.player_label[2] = data['request_params']['player2_label']
 
-    def valid_actions(self):
-        """
-        The valid actions are the empty spaces.
-        """
-        valids = []
-        for index, space in enumerate(self.inputs_):
-            if space == TictactoeGame.EMPTY:
-                valids.append(index)
-        return valids
+        self.inputs_ = [TictactoeGame.EMPTY, TictactoeGame.EMPTY, TictactoeGame.EMPTY,
+                        TictactoeGame.EMPTY, TictactoeGame.EMPTY, TictactoeGame.EMPTY,
+                        TictactoeGame.EMPTY, TictactoeGame.EMPTY, TictactoeGame.EMPTY]
+        self.result_ = -1
 
-    def inputs_from_the_point_of_view_of(self, position):
+    def _inputs_from_the_point_of_view_of(self, position):
         """
         Get a copy of the inputs so that the player in 'position' always see the board with 
         spaces with '1' as their spaces, and '2' as the opponent's spaces.
@@ -141,12 +149,34 @@ class TictactoeGame():
             inputs = [mapping[x]*TictactoeGame.CONFIG['multiply_normalization_by'] for x in list(self.inputs_)]
             return inputs
 
-    def is_over(self):
+    def _valid_actions(self):
+        """
+        The valid actions are the empty spaces.
+        """
+        valids = []
+        for index, space in enumerate(self.inputs_):
+            if space == TictactoeGame.EMPTY:
+                valids.append(index)
+        return valids
+
+    def _perform_action(self, current_player, action):
+        """
+        Perform the action for the current_player in the board, modifying 
+        the attribute inputs_.
+        """
+        self.inputs_[action] = current_player
+        if self.print_game_:
+            print "---"
+            print str(self.inputs_[0:3])
+            print str(self.inputs_[3:6])
+            print str(self.inputs_[6:9])
+
+    def _is_over(self):
         """
         Check if all spaces were used. If yes, sets the attribute result_ with the 
         number of the winner or 0 if a draw occured.
         """
-        winner = TictactoeGame.get_winner(self.inputs_)
+        winner = self._get_winner()
         if winner:
             self.result_ = winner
             if self.print_game_:
@@ -162,23 +192,22 @@ class TictactoeGame():
             print "It is over! Draw!"
         return True
 
-    def result_for_player(self, current_player):
+    def _get_winner(self):
+        winning_configs = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+                       (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        for config in winning_configs:
+            if self.inputs_[config[0]] == self.inputs_[config[1]] and self.inputs_[config[1]] == self.inputs_[config[2]]:
+                if self.inputs_[config[0]] != 0:
+                    return self.inputs_[config[0]]
+        return None # no winner
+
+    def _result_for_player(self, current_player):
         if self.result_ == current_player:
             return 1 # win
         if self.result_ == TictactoeGame.DRAW:
             return 0.5 # draw
         else:
             return 0 # lose
-
-    @staticmethod
-    def get_winner(inputs):
-        winning_configs = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
-                       (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-        for config in winning_configs:
-            if inputs[config[0]] == inputs[config[1]] and inputs[config[1]] == inputs[config[2]]:
-                if inputs[config[0]] != 0:
-                    return inputs[config[0]]
-        return None # no winner
 
 if __name__ == "__main__":
     test_mode = False
